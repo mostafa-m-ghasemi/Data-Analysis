e2868d232688741df93bca9073ddc0b54796d946
Mostafa <mostafa.m.qasemi@gmail.com>
2026-07-24 13:45:49 +0330

    Add files via upload

---
+++ Libraries/Numpy/Mathematical.py
@@ -0,0 +1,11 @@
+import numpy as np
+data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+data = np.array(data)
+
+print(f"sqrt : {np.sqrt(data)}")
+print("-" * 30)
+print(f"square root : {np.square(data)}")
+print("-" * 30)
+print(f"power : {np.power(data, 2)}")
+print("-" * 30)
+print(f"abs : {np.abs(data)}")  # قدر مطلق
