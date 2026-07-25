acdb2d9703784b722f5896df0fcb925d9e5f0a80
Mostafa <mostafa.m.qasemi@gmail.com>
2026-07-25 07:38:01 +0330

    Add files via upload

---
+++ Libraries/Numpy/Trigonometric.py
@@ -0,0 +1,16 @@
+import numpy as np
+data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+data = np.array(data)
+
+print(f"sin : {np.sin(data)}")
+print("-" * 30)
+print(f"cos : {np.cos(data)}")
+print("-" * 30)
+print(f"tan: {np.tan(data)}")
+print("-" * 30)
+print("-" * 30)
+print(f"arcsin : {np.arcsin(data)}")
+print("-" * 30)
+print(f"arccos : {np.arccos(data)}")
+print("-" * 30)
+print(f"arctan: {np.arctan(data)}")
