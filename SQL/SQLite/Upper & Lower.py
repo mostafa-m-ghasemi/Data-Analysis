22dd8a5704c1cbaede2e347f9e109ecf31224d75
Mostafa <mostafa.m.qasemi@gmail.com>
2026-07-31 10:54:55 +0330

    Add files via upload

---
+++ SQL/SQLite/Upper & Lower.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect("WSDA_Music.db")
+cursor = conn.cursor()
+query = ("""SELECT FirstName, LastName, Address,
+FirstName ||' '|| LastName ||' '|| Address ||',' || City || ',' || State || ' ' || PostalCode AS [Mailling addres],
+LENGTH(PostalCode),
+substr(PostalCode,1,5) AS [5 Digit PostalCode],
+upper(firstname),
+lower(lastname)
+FROM Customer
+where Country = 'USA'"""
+         )
+# Using of () is really important!!!
+cursor.execute(query)
+result = cursor.fetchall()
+for row in result:
+    print(row)
+conn.close()
