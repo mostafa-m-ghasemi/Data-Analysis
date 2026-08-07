e67488f9a913731704d5340af9f691079b4eedfb
Mostafa <mostafa.m.qasemi@gmail.com>
2026-08-07 10:42:47 +0330

    Update and rename SQL/SQLite/Aggregate () Functions.py to SQL/SQLite/Functions/Aggregate () Functions.py

--- SQL/SQLite/Functions/Aggregate () Functions.py
+++ SQL/SQLite/Functions/Aggregate () Functions.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect("WSDA_Music.db")
 cursor = conn.cursor()
-query = ("""SELECT SUM(total) AS [Toyal Sales], AVG(total) AS [Avrage Sales], MAX(total) AS [Maximun Sales],
+query = ("""SELECT SUM(total) AS [Total Sales], AVG(total) AS [Avrage Sales], MAX(total) AS [Maximun Sales],
 MIN(total) AS [Minumum Sales], COUNT(total) AS [Total count of Sales]
 FROM Invoice
 """
