71f9506e2d2605fe806ace6b206b0f0c2c155c12
Mostafa <mostafa.m.qasemi@gmail.com>
2026-08-07 10:49:18 +0330

    Add files via upload

---
+++ SQL/SQLite/Group by/Group by.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect("WSDA_Music.db")
+cursor = conn.cursor()
+query = ("""SELECT BillingCity, ROUND(AVG(total), 2)
+FROM Invoice
+GROUP BY BillingCity
+ORDER BY BillingCity
+"""
+         )
+# Using of () is really important!!!
+cursor.execute(query)
+result = cursor.fetchall()
+for row in result:
+    print(row)
+conn.close()
